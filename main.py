5e61acf55af2d408dd9a2cb9625c78ec95683840
successor1 <kyildirim2002abc@gmail.com>
2021-08-04 16:36:23 +0200

    update

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QDesktopServices
 from views.view_ui import Ui_mainWindow
+from views.about_window import Ui_Form
 import sys
 from models.model import Model
 
-class QrlWallet(QtWidgets.QMainWindow, Ui_mainWindow):
+class QrlWallet(QtWidgets.QMainWindow, Ui_mainWindow, Ui_Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -15,7 +16,9 @@ class QrlWallet(QtWidgets.QMainWindow, Ui_mainWindow):
         self.model = Model()
 
         self.send_button.clicked.connect(self.button_clicked)
+        self.actionAbout.triggered.connect(self.about_popup)
         self.actionOfficial_website.triggered.connect(lambda: QDesktopServices.openUrl(QUrl("https://theqrl.org")))
+        self.actionQRL_whitepaper.triggered.connect(lambda: QDesktopServices.openUrl(QUrl("https://docs.theqrl.org/")))
         self.actionQRL_whitepaper.triggered.connect(lambda: QDesktopServices.openUrl(QUrl("https://raw.githubusercontent.com/theQRL/Whitepaper/master/QRL_whitepaper.pdf")))
 
 
@@ -25,6 +28,13 @@ class QrlWallet(QtWidgets.QMainWindow, Ui_mainWindow):
 
     def update(self):
         self.balance_label.adjustSize()
+
+    def about_popup(self):
+        self.dialog=QtWidgets.QMainWindow()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self.dialog)
+        self.dialog.show()
+
 
 
 if __name__ == "__main__":
