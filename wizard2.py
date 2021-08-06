e0bd2e0b8ccafc5efe3236e989c393a8441a61d8
successor1 <kyildirim2002abc@gmail.com>
2021-08-06 21:24:10 +0200

    update

--- wizard2.py
+++ wizard2.py
@@ -1,43 +1,38 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from main import QrlWallet
 
 class MyWizard(QtWidgets.QWizard):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-
         self.introPage = IntroPage()
         self.firstPageOptionA = FirstPageOptionA()
+        self.SecondPageOptionA = SecondPageOptionA()
+        self.secondPageOptionB = SecondPageOptionB()
+        self.thirdPageOptionC = ThirdPageOptionC()
         self.lastPage = LastPage()
         self.addPage(self.introPage)
         self.addPage(self.firstPageOptionA)
+        self.addPage(self.SecondPageOptionA)
+        self.addPage(self.secondPageOptionB)
+        self.addPage(self.thirdPageOptionC)
         self.addPage(self.lastPage)
-        # self.setPage(0, self.introPage)
-        # self.setPage(1, self.firstPageOptionA)
-        # self.setPage(2, self.lastPage)
-
-        self.button(QWizard.NextButton).clicked.connect(self._nextPage)
-    def _nextPage(self):
-        if self.currentPage() == self.introPage:
-            if self.introPage == self.introPage.radiobutton_1.isChecked():
-                return self.currentId() + 2
-        # if self.introPage.radiobutton_1.isChecked():
-        #     return self.currentId + 2
-
 
 
 class IntroPage(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
-        
+
         self.setTitle("Welcome to QRL Light Wallet!")
 
         self.label_description = QLabel("Select option:")
         self.radiobutton_1 = QRadioButton("Create new wallet")
         self.radiobutton_2 = QRadioButton("Open wallet file")
         self.radiobutton_3 = QRadioButton("Restore wallet from seed")
+        self.radiobutton_2.setChecked(True)
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.label_description)
@@ -45,33 +40,102 @@ class IntroPage(QtWidgets.QWizardPage):
         layout.addWidget(self.radiobutton_2)
         layout.addWidget(self.radiobutton_3)
 
+    def nextId(self) -> int:
+        if self.radiobutton_1.isChecked():
+            return 1
+        if self.radiobutton_2.isChecked():
+            return 3
+        if self.radiobutton_3.isChecked():
+            return 4
+
+        return 0
+
 class FirstPageOptionA(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setTitle("Create wallet!")
 
-        self.NameLabel = QLabel("&Name:")
-        self.NameLineEdit = QLineEdit()
-        self.NameLabel.setBuddy(self.NameLineEdit)
+        self.name_label = QLabel("Name:")
+        self.nameline_edit = QLineEdit()
+        self.password_label = QLabel("Password (optional):")
+        self.passwordline_edit = QLineEdit()
+        self.directory_label = QLabel("Directory:")
+        self.directoryline_edit = QLineEdit()
 
-        layout = QHBoxLayout(self)
-        layout.addWidget(self.NameLabel)
-        layout.addWidget(self.NameLineEdit)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.nameline_edit)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.passwordline_edit)
+        layout.addWidget(self.directory_label)
+        layout.addWidget(self.directoryline_edit)
+
+    def nextId(self) -> int:
+        return 2
+
+class SecondPageOptionA(QtWidgets.QWizardPage):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTitle("Wallet seed")
+
 
 class SecondPageOptionB(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setTitle("Open wallet file")
 
+        # self.openFileBtn = QtWidgets.QPushButton("Import wallet key file")
+        # layout = QtWidgets.QVBoxLayout()
+        # layout.addWidget(self.openFileBtn)
+        # self.setLayout(layout)
+        # self.openFileBtn.clicked.connect(self.openFileNameDialog)
+
+        self.model = QFileSystemModel()
+        self.model.setRootPath(r'C:\Users\31622\Documents')
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setRootIndex(self.model.index(r'C:\Users\31622\Documents'))
+        
+        self.tree.setAnimated(False)
+        self.tree.setIndentation(20)
+        self.tree.setSortingEnabled(True)
+        self.tree.setWindowTitle("Dir View")
+        self.tree.resize(640, 480)
+        
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.tree)
+        self.setLayout(windowLayout)
+
+    @QtCore.pyqtSlot()
+    def openFileNameDialog(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, "QFileDialog.getOpenFileName()", "",
+            "All Files (*);;Python Files (*.py)", options=options)
+        # if user selected a file store its path to a variable
+        if fileName:
+            self.wizard().variable = fileName
 
 class ThirdPageOptionC(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setTitle("Restore your wallet")
+
+        self.seed_label = QLabel("Enter your seed:")
+        self.seedline_edit = QLineEdit()
+
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.seed_label)
+        layout.addWidget(self.seedline_edit)
+
 
 class LastPage(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.NameLabel = QLabel("&ummm:")
+        self.NameLabel = QLabel("last page:")
         self.NameLineEdit = QLineEdit()
         self.NameLabel.setBuddy(self.NameLineEdit)
 
@@ -84,7 +148,7 @@ class LastPage(QtWidgets.QWizardPage):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     main = MyWizard()
-    mainWindow = QrlWallet()
+    mainWindow = QrlWallet()  
     main.hide()
     main.setWindowModality(QtCore.Qt.ApplicationModal)
     main.show()
