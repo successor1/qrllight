4a3caa425c2857f4f2a56d97a3d471a2d6194ff9
succesor1 <kyildirim2002abc@gmail.com>
2021-08-09 21:33:21 +0200

    update

--- models/model.py
+++ models/model.py
@@ -1,62 +1,39 @@
-# model.py
-# D. Thiebaut
-# This is the model part of the Model-View-Controller
-# The class holds the name of a text file and its contents.
-# Both the name and the contents can be modified in the GUI
-# and updated through methods of this model.
-# 
+import os
+import numpy as np
+import requests
+import json
+from pyqrllib.pyqrllib import str2bin, XmssFast, mnemonic2bin, hstr2bin, bin2hstr, SHAKE_128, SHAKE_256, SHA2_256, getRandomSeed
+from qrl.core.misc import logger
+from qrl.crypto.xmss import XMSS
+from qrl.crypto.xmss import XMSS, hash_functions
+from qrl.core.Wallet import Wallet, WalletDecryptionError
+
 
 class Model:
-    def __init__( self ):
-        '''
-        Initializes the two members the class holds:
-        the file name and its contents.
-        '''
-        self.fileName = None
-        self.fileContent = ""
+    def __init__(self):
+        pass
 
-    def isValid( self, fileName ):
-        '''
-        returns True if the file exists and can be
-        opened.  Returns False otherwise.
-        '''
-        try: 
-            file = open( fileName, 'r' )
-            file.close()
-            return True
-        except:
-            return False
+    def getAddress():
+        xmss_height = 10
+        seed = getRandomSeed(48, '')
+        xmss = XMSS(XmssFast(seed, xmss_height))
+        print(xmss.qaddress)
+        print(xmss.mnemonic)
+        print(xmss.hexseed)
 
-    def setFileName( self, fileName ):
-        '''
-        sets the member fileName to the value of the argument
-        if the file exists.  Otherwise resets both the filename
-        and file contents members.
-        '''
-        if self.isValid( fileName ):
-            self.fileName = fileName
-            self.fileContents = open( fileName, 'r' ).read()
-        else:
-            self.fileContents = ""
-            self.fileName = ""
-            
-    def getFileName( self ):
-        '''
-        Returns the name of the file name member.
-        '''
-        return self.fileName
+    def recoverAddress():
+        seed = ""
+        bin_seed = mnemonic2bin(seed)
+        recovered_xmss = XMSS.from_extended_seed(bin_seed)
+        print(recovered_xmss.qaddress)
 
-    def getFileContents( self ):
-        print("Test!")
-    
-    def writeDoc( self, text ):
-        '''
-        Writes the string that is passed as argument to a
-        a text file with name equal to the name of the file
-        that was read, plus the suffix ".bak"
-        '''
-        if self.isValid( self.fileName ):
-            fileName = self.fileName + ".bak"
-            file = open( fileName, 'w' )
-            file.write( text )
-            file.close()
+    def getAddressBalance():
+        address = "Q01050048f13d469bd98bd83ae31427e6fd980a299016cfbbfe6802e36123c42e98350d4103e983"
+        request = requests.get('https://explorer.theqrl.org/api/a/'+address)
+        response = request.text
+        getAddressResp = json.loads(response)
+        jsonResponse = getAddressResp
+        print(jsonResponse)
+
+
+Model.getAddressBalance()
