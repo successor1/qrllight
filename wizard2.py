8202f377f977bbadbbc9024e4b716b3d57698050
succesor1 <kyildirim2002abc@gmail.com>
2021-08-10 23:14:03 +0200

    wallet creation UI

--- wizard2.py
+++ wizard2.py
@@ -12,6 +12,7 @@ class MyWizard(QtWidgets.QWizard):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setFixedSize(550, 400)
         self.introPage = IntroPage()
         self.firstPageOptionA = FirstPageOptionA(self)
         self.SecondPageOptionA = SecondPageOptionA()
@@ -29,9 +30,19 @@ class MyWizard(QtWidgets.QWizard):
         self.currentIdChanged.connect(self.next_callback)
 
 
+
     def next_callback(self, page_id: int):
         if page_id == 2 and self.last_page_id == 1:
-            return Model.getAddress(10, SHAKE_256)
+            combo_height_short = self.firstPageOptionA.combo_height
+            combo_hash_short = self.firstPageOptionA.combo_hash
+            if combo_height_short.currentIndexChanged or combo_hash_short.currentIndexChanged:
+                combo_height_options = {0: 8, 1: 10, 2: 12, 3: 14, 4: 16, 5: 18}
+                combo_hash_options = {0: SHAKE_128, 1: SHAKE_256, 2: SHA2_256}
+            qaddress, mnemonic, hexseed = Model.getAddress(combo_height_options[combo_height_short.currentIndex()], combo_hash_options[combo_hash_short.currentIndex()])
+            self.SecondPageOptionA.qaddress.setText(qaddress)
+            self.SecondPageOptionA.mnemonic.setText(mnemonic + "\n" + "\n")
+            self.SecondPageOptionA.hexseed.setText(hexseed)
+            # if page_id == 4 and self.last_page_id == 3:
         self.last_page_id = page_id
 
 
@@ -84,7 +95,7 @@ class FirstPageOptionA(QtWidgets.QWizardPage):
         self.combo_hash = QComboBox(self)
         self.combo_hash.addItem("Hash function: SHAKE_128")
         self.combo_hash.addItem("Hash function: SHAKE_256")
-        self.combo_hash.addItem("Hash function: SHA2_256")
+        self.combo_hash.addItem("Hash function: SHA2_256") 
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.password_label)
@@ -92,17 +103,46 @@ class FirstPageOptionA(QtWidgets.QWizardPage):
         layout.addWidget(self.combo_height)
         layout.addWidget(self.combo_hash)
 
-
-
-
     def nextId(self) -> int:
         return 2
 
 class SecondPageOptionA(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setTitle("Wallet seed")
+        self.setTitle("Wallet details")
 
+        self.qaddress_description = QLabel("QRL Public address:")
+        self.qaddress = QTextEdit()
+        self.qaddress.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        self.mnemonic_description = QLabel("Mnemonic phrase:")
+        self.mnemonic = QLabel()
+        self.hexseed_description = QLabel("Hexseed:")
+        self.hexseed = QLabel()
+
+        self.scroll = QScrollArea()
+        self.scroll.setWidgetResizable(True)
+
+        self.frame = QFrame(self)
+        self.qaddress.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        self.qaddress.setFrameShadow(QFrame.Plain)
+        self.qaddress.setLineWidth(1)
+
+        self.mnemonic.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        self.mnemonic.setFrameShadow(QFrame.Plain)
+        self.mnemonic.setLineWidth(1)
+        self.mnemonic.setWordWrap(True)
+
+        self.hexseed.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        self.hexseed.setFrameShadow(QFrame.Plain)
+        self.hexseed.setLineWidth(1)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.qaddress_description)
+        layout.addWidget(self.qaddress)
+        layout.addWidget(self.mnemonic_description)
+        layout.addWidget(self.mnemonic)
+        layout.addWidget(self.hexseed_description)
+        layout.addWidget(self.hexseed)
 
 class SecondPageOptionB(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
